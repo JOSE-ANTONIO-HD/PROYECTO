0943df712ff9725cf86d98c1ec9a2faee0833858
JOSÉ-ANTONIO-HD <hdzdomjoseantonio@gmail.com>
2024-04-14 03:13:57 -0600

    de nuevo

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
 export default Axios.create({
-    baseURL:"http://127.0.0.1:4000/api"
+    baseURL:"https://api.render.com/deploy/srv-coc5mpu3e1ms73av6leg?key=ynhNJR60gtM"
 })
 
